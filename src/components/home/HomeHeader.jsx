ce88fb764c4f1456e36b3ca1b5c52555cc6cbf79
jemikanegara <jemikanegara@gmail.com>
2023-08-26 07:37:23 +0800

    remove assets & use store for home text

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -1,9 +1,12 @@
 import { Box } from "@mui/material"
+import useStore from "../../store"
 
 export default function HomeHeader() {
+  const homeText = useStore(state => state.staticStore.homeText)
+
   return (
     <Box className="bg-black p-5 max-w-xs mx-auto text-white">
-      HomeHeader component
+      {homeText}
     </Box>
   )
 }
--- src/store/staticStore.jsx
+++ src/store/staticStore.jsx
@@ -1,5 +1,6 @@
 const staticStore = (set, get, api) => ({
   apiUrl: "", // replace with your API URL
+  homeText: "Welcome to home",
 })
 
 export default staticStore
