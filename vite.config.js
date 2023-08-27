b591bd7ddd7b00c751de7808201ff4eccb936807
donaderoyan <donaderoyan@gmail.com>
2023-08-27 22:14:40 +0700

    add base url gh page

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
         { find: '@', replacement: path.resolve(__dirname, 'src') },
     ],
   },
-  // base: '/react-mui-zustand-boiler/'
+  base: '/react-mui-zustand-boiler/'
 })
